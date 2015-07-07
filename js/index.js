9cce8e7d9906ed98af150d7e59d339105e6ce629
jysdu2006 <jysdu2006@gmail.com>
2015-07-07 16:45:05 +0800

    201507071645

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ function get_part(){
 		get_machine();
 	});
 }
-get_part();
+
 function full_cont(){
 	if(partcount>0){
 		var machine_list='';
@@ -21,6 +21,7 @@ function full_cont(){
 			machine_list+='<div class="childpane" id="'+temp+'"><div class="underline">'+partname[i]+'</div></div>';
 		}
 		var $list=$(machine_list);
+		$('.spidepane').empty();
 		$list.appendTo($('.spidepane'));
 	}
 }
@@ -31,9 +32,10 @@ function get_machine(){
 				var data=eval(jsondata);
 				var temp="";
 				var id=data[data.length-1];
-				for(var j=0;j<data.length-1;j=j+3)
-					temp+='<div class="showtime" id="machine'+id+j+'">'+data[j]+'<br/><br/>'+data[j+1]+'<br/>'+data[j+2]+'</div>';
+				for(var j=0;j<data.length-1;j=j+4)
+					temp+='<div class="showtime" id="machine'+id+j+'">'+data[j]+'<br/><br/>'+data[j+1]+'<br/>'+data[j+2]+'<br/>'+data[j+3]+'</div>';
 				var $machine=$(temp);
+				$('#group'+data[data.length-1]).empty();
 				$machine.appendTo($('#group'+data[data.length-1]));
 			});
 		}
@@ -43,11 +45,20 @@ function get_machine(){
 function execute_time(){
 	$.get('/newclock/server/info.php',{type:3},function(jsondata){
 		var data=eval(jsondata);
-		var temp="";
+		var temp="<p>";
 		for(var i=0;i<data.length;i=i+3)
-			temp+=data[i]+"<br/>"+data[i+1]+"<br/>"+data[i+2];
-		$('#standtime').text(temp);
+			temp+=data[i]+'<br/>'+data[i+1]+'<br/>'+data[i+2];
+		temp+='</p>';
+		var $temp=$(temp);
+		$('#standtime').empty();
+		$temp.appendTo($('#standtime'));
 	});
 }
 
+
+get_part();
+
+setInterval(get_part(),10000);
 execute_time();
+setInterval(execute_time(),10000);
+
--- server/info.php
+++ server/info.php
@@ -24,6 +24,48 @@ if($type==='2'){
 		$arr=explode(" ", $temp);
 		$data[]=$arr[0];
 		//print_r(length($arr));
+		$resultfile=fopen("d:/clock/result/".$arr[0].".txt",'r');
+		$result=fgets($resultfile);
+		$rearr=explode(' ', $result);
+		$data[]=$rearr[1];
+		$data[]=$rearr[2];
+		$data[]=$rearr[3];
+	}
+	fclose($file);
+	$data[]=$num;
+	echo(json_encode($data));
+}
+
+
+if($type==='3'){
+	$filename='d:/clock/result/stand.txt';
+	$data=array();
+	$file=fopen($filename,'r');
+	$data[]='基准时间:';
+	while(!feof($file)){
+		$temp=fgets($file);
+		$arr=explode(" ", $temp);
+		//$command='java -jar d://webtime.jar '.$arr[0].' '.$arr[1].' '.$arr[2].' '.$arr[3];
+		//exec($command,$out);
+		$data[]=$arr[1];
+		$data[]=$arr[2];
+	}
+	fclose($file);
+	echo(json_encode($data));
+}
+
+/*
+if($type==='2'){
+	$num=$_GET['num'];
+	$filename="../cfg/info".$num.".txt";
+	//$filename="../cfg/info1.txt";
+	$data=array();
+	$file=fopen($filename,'r');
+	while(!feof($file)){
+		$temp=fgets($file);
+		$arr=explode(" ", $temp);
+		$data[]=$arr[0];
+		//print_r(length($arr));
 		$command='java -jar d://webtime.jar '.$arr[0].' '.$arr[1].' '.$arr[2].' '.$arr[3];
 		//print_r($data[1]." ".$data[2]." ".$data[3]);
 		//$command="java -jar d://webtime.jar 127.0.0.1 1 administrator jinying";
@@ -52,7 +94,7 @@ if($type==='3'){
 	fclose($file);
 	echo(json_encode($data));
 }
-
+*/
 
 
 
